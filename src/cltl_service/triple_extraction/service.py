61373e886519f0aac8ee5604158f442ff560279f
Thomas Baier <tbaier@math.bme.hu>
2022-04-21 09:45:27 +0200

    Add name to topic worker

--- src/cltl_service/triple_extraction/service.py
+++ src/cltl_service/triple_extraction/service.py
@@ -44,7 +44,8 @@ class TripleExtractionService:
 
     def start(self, timeout=30):
         self._topic_worker = TopicWorker([self._input_topic], self._event_bus, provides=[self._output_topic],
-                                         resource_manager=self._resource_manager, processor=self._process)
+                                         resource_manager=self._resource_manager, processor=self._process,
+                                         name=self.__class__.__name__)
         self._topic_worker.start().wait()
 
     def stop(self):
