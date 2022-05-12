4e5d6bc46064053f30c013e71e5e84161f97ba6d
Selene Baez <s.baezsantamaria@vu.nl>
2022-05-12 16:39:11 +0200

    move language data to commons

--- src/cltl/triple_extraction/data/base_cases.py
+++
@@ -1,3 +0,0 @@
-name = 'Leolani'
-places = ['Forest', 'Playground', 'Monastery', 'House', 'University', 'Hotel', 'Office']
-friends = ['Piek', 'Lenka', 'Bram', 'Suzana', 'Selene', 'Lea', 'Thomas', 'Jaap', 'Tae']
