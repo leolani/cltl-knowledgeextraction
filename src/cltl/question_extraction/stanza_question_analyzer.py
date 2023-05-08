dcbb2200d5c6c0a2710d3c954c4036b82911257f
piekvossen <piekvossen@gmail.com>
2023-05-08 15:47:14 +0200

    -- new module to process a question by first converting it to a statement, next extracting triples and finally replacing one element by a question variable. E.g. "Who do you like?" --> You like **blanks** (statement)" --> triple [subj:you, predicate: like, obj: **blanks**] --> triple [subj:you, predicate: like, obj: ?]

--- src/cltl/question_extraction/stanza_question_analyzer.py
+++ src/cltl/question_extraction/stanza_question_analyzer.py
@@ -1,8 +1,8 @@
 import stanza
 from cltl.question_extraction.question_to_statement import POSTree
 from cltl.question_extraction.analyzer import Analyzer
-#from cltl.triple_extraction.api import Chat
-#from cltl.triple_extraction.cfg_analyzer import CFGAnalyzer
+from cltl.triple_extraction.api import Chat
+from cltl.triple_extraction.cfg_analyzer import CFGAnalyzer
 
 # 1. Call stanza to parse a text: https://stanfordnlp.github.io/stanza/data_conversion.html#document-to-python-object
 # 2. If question, use POSTree to convert it to a statement
@@ -21,15 +21,15 @@ class StanzaQuestionAnalyzer(Analyzer):
         """
         self._parser = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,constituency')
         self._utterance = None
-        self._statements = []
+        self._triples = []
 
     @property
     def utterance(self):
         return self._utterance
 
     @property
-    def statements(self):
-        return self._statements
+    def triples(self):
+        return self._triples
 
     def analyze(self, utterance):
         """
@@ -42,10 +42,32 @@ class StanzaQuestionAnalyzer(Analyzer):
         """
         self._utterance = utterance
         doc = self._parser(self._utterance)
+        statements = []
         for sentence in doc.sentences:
             tree = POSTree(str(sentence.constituency))
-            statement = tree.adjust_order().replace("**blank**", "It")
-            self.statements.append(statement)
+            statement = tree.adjust_order().replace("**blank**", "DUMMY")
+            statements.append(statement)
+        for statement in statements:
+            #### Extract the triples
+            chat = Chat("Leolani", "Lenka")
+            triple_extractor = CFGAnalyzer()
+            chat.add_utterance(statement)
+            triple_extractor.analyze(chat.last_utterance)
+            for triple in chat.last_utterance.triples:
+                DUMMY = False
+                if triple["subject"]=="DUMMY":
+                    triple["subject"]=="?"
+                    DUMMY = True
+                if triple["predicate"]=="DUMMY":
+                    triple["predicate"]=="?"
+                    DUMMY = True
+                if triple["object"]=="DUMMY":
+                    triple["object"]=="?"
+                    DUMMY = True
+                if DUMMY:
+                    self._triples.append(triple)
+
+
 
 if __name__ == "__main__":
 
@@ -53,5 +75,5 @@ if __name__ == "__main__":
     text = 'What tracks users?'
     analyzer.analyze(text)
     print(analyzer.utterance)
-    print(analyzer.statements)
+    print(analyzer.triples)
 
