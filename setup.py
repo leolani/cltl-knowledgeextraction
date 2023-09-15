29ab40bfc8dc9dfc381f5675df6d488bf26ea39d
Thomas Baier <tbaier@math.bme.hu>
2023-09-15 11:15:14 +0200

    Loosen dependency bounds

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ setup(
     ]},
     python_requires='>=3.7',
     install_requires=[
-        'nltk~=3.4.4',
-        'stanford_openie~=1.3.0',
-        'spacy~=3.2.3',
+        'nltk~=3.4',
+        'stanford_openie~=1.3',
+        'spacy~=3.2',
         'stanza~=1.5',
         "cltl.combot"
     ],
